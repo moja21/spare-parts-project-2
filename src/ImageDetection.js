2f931367a045d7ac1544c6b029324429fb550cfa
Moja kh <mojahedskh@gmail.com>
2023-06-12 21:30:52 +0300

    image detection fixes

--- src/ImageDetection.js
+++ src/ImageDetection.js
@@ -1,55 +1,105 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-
+import './ImageDetection.css';
 const ImageDetection = () => {
-  const [selectedImage, setSelectedImage] = useState(null);
-  const [detectionResult, setDetectionResult] = useState(null);
+  const [imageUrl, setImageUrl] = useState('');
+  const [userImage, setUserImage] = useState(null);
+  const [apiImage, setApiImage] = useState(null);
+  const [detections, setDetections] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
 
-  const handleImageUpload = async (e) => {
-    const file = e.target.files[0];
-    setSelectedImage(URL.createObjectURL(file));
+  const options = {
+    method: 'POST',
+    url: 'https://vehicle-damage-assessment.p.rapidapi.com/run',
+    headers: {
+      'content-type': 'application/json',
+      'X-RapidAPI-Key': '1827ee75a6msh688da8bb5d1d46fp17d3bejsn9020a7831e64',
+      'X-RapidAPI-Host': 'vehicle-damage-assessment.p.rapidapi.com'
+    },
+    data: {
+      draw_result: true,
+      remove_background: false,
+      image: imageUrl
+    }
+  };
 
-    const formData = new FormData();
-    formData.append('image', file);
-
-    const options = {
-      method: 'POST',
-      url: 'https://vehicle-damage-assessment.p.rapidapi.com/run',
-      headers: {
-        'content-type': 'application/json',
-        'X-RapidAPI-Key': '1827ee75a6msh688da8bb5d1d46fp17d3bejsn9020a7831e64',
-        'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
-      },
-      data: {
-        draw_result: false,
-        remove_background: false,
-        image: URL.createObjectURL(file)
-      }
-    };
+  const performImageDetection = async () => {
+    setIsLoading(true);
+    setErrorMessage('');
 
     try {
-      setIsLoading(true);
       const response = await axios.request(options);
-      setDetectionResult(response.data);
+      const { output } = response.data;
+      setDetections(output.elements);
+      setApiImage(response.data.output_url);
     } catch (error) {
-      console.error(error);
+      console.error('Error detecting image:', error);
+      setErrorMessage('An error occurred while detecting the image.');
     } finally {
       setIsLoading(false);
     }
   };
 
+  const handleDetectClick = () => {
+    if (imageUrl) {
+      setUserImage(imageUrl);
+      performImageDetection();
+    } else {
+      setErrorMessage('Please enter a valid image URL.');
+    }
+  };
+
+  const handleInputChange = (event) => {
+    setImageUrl(event.target.value);
+  };
+
   return (
-    <div className='image-detection'>
-      <h1>Image Detection</h1>
-      <input type="file" onChange={handleImageUpload} />
-      {selectedImage && <img src={selectedImage} alt="Selected" />}
-      {isLoading && <p>Loading...</p>}
-      {detectionResult && (
+    <div className="image-detection-container">
+      <input
+        type="text"
+        value={imageUrl}
+        onChange={handleInputChange}
+        placeholder="Image URL"
+        className="image-input"
+      />
+      <button onClick={handleDetectClick} className="detect-button">
+        Detect
+      </button>
+
+      {isLoading && <p>Connecting to API...</p>}
+
+      {userImage && (
         <div>
-          <h2>Detection Result</h2>
-          <p>{JSON.stringify(detectionResult)}</p>
+          <h2>User Image:</h2>
+          <img src={userImage} alt="User" className="image-preview" />
         </div>
+      )}
+
+      {apiImage && (
+        <div>
+          <h2>API Result Image:</h2>
+          <img src={apiImage} alt="API Result" className="image-preview" />
+        </div>
+      )}
+
+      {errorMessage && <p>{errorMessage}</p>}
+
+      {detections.length > 0 ? (
+        <div>
+          <h2>Image Detections:</h2>
+          <ul>
+            {detections.map((element, index) => (
+              <li key={index}>
+                <p>Bounding Box: {element.bbox}</p>
+                <p>Damage Category: {element.damage_category}</p>
+                <p>Damage Location: {element.damage_location}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ) : (
+        <p>No detections found.</p>
       )}
     </div>
   );
